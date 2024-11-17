eea2c596dacc61eadbadc48d040f9e57c6682901
XD-cods <vkuzir7@gmail.com>
2024-11-17 12:29:08 +0300

    update AppliationConfig

--- task-service/src/main/java/com/vlad/taskservice/config/ApplicationConfig.java
+++ task-service/src/main/java/com/vlad/taskservice/config/ApplicationConfig.java
@@ -1,7 +1,5 @@
 package com.vlad.taskservice.config;
 
-import com.vlad.taskservice.persistance.entity.Task;
-import com.vlad.taskservice.web.request.TaskRequest;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +12,6 @@ public class ApplicationConfig {
   public ModelMapper modelMapper() {
     ModelMapper modelMapper = new ModelMapper();
 
-    // Устанавливаем условие, чтобы игнорировать null значения
     modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
     return modelMapper;
   }
